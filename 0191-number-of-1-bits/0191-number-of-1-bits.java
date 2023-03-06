f2de0df539d8a0ae0dbb84e1568cae69a8fe3366
BASWANI PRANEETHA <120731376+pranee2003@users.noreply.github.com>
2023-03-06 21:20:25 +0530

    Time: 1 ms (31.14%), Space: 39.8 MB (23.43%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -4,10 +4,10 @@ public class Solution {
     int count = 0;
 
         while (n != 0) {
-            if ((n & 1) == 1)
+            if ((n & 1)== 1)
                 count++;
             
-            n = n >>> 1;
+            n = n >>>1;
         }
             
         return count;
