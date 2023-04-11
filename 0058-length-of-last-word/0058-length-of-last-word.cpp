0c6c16935b3660903b59d1d74be285eb2ee857b7
BASWANI PRANEETHA <120731376+pranee2003@users.noreply.github.com>
2023-04-11 23:27:03 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (58.96%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+   int lengthOfLastWord(string s) {
+    int i= s.length()-1;
+       int c=0;
+       while(i>=0 && s[i]==' ')
+       {
+           i--;
+       }
+        while(i>=0 && s[i]!=' ')
+        {
+            c++;
+            i--;
+        }
+       return c;
+   }
+};
+       
