d9be30dbee30a952713790aad83a32cc8dab56dc
BASWANI PRANEETHA <bpraneetha17@gmail.com>
2024-05-18 19:34:10 +0530

    Time: 2 ms (96.36%), Space: 41.8 MB (39.65%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i : arr)
+        {
+            map.put(i,map.getOrDefault(i,0)+1);            
+        }
+        HashSet<Integer> unique= new HashSet(map.values());
+        return map.size()==unique.size();
+    }
+}
