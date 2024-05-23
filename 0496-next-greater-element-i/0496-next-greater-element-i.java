f6067486a0791de2413c9b0818b77404217135bf
BASWANI PRANEETHA <bpraneetha17@gmail.com>
2024-05-23 13:57:49 +0530

    Time: 3 ms (90.58%), Space: 43.5 MB (58.19%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -2,9 +2,6 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Map<Integer,Integer> map=new HashMap<>();
         Stack<Integer> s =new Stack<>();
-
-        // if(nums1.length==0 || nums2.length==0)
-        //     return new int[0];
         for(int i=nums2.length-1;i>=0;i--){
         while(!s.isEmpty() && s.peek()<=nums2[i]){
             s.pop();
